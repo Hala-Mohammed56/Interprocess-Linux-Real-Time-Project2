8116942c0a98c975b8c8c7a484f72611e6ea2dcc
Hala Mohammed <halaabed56@gmail.com>
2025-04-25 19:42:52 +0300

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,1 +1,19 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+// Standard C Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+
+// IPC-related
+#include <sys/stat.h>     // For mkfifo
+#include <sys/mman.h>     // For shared memory
+#include <semaphore.h>    // For semaphores
+
+#endif
 
