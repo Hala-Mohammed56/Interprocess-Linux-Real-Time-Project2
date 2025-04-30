f47f9d742821f7338e722e5563b8eb8fe25fd298
Obayda Sarraj <1211128@student.birzeit.edu>
2025-04-30 19:01:36 +0300

    semaphore.c

---
+++ semaphore.c
@@ -0,0 +1,44 @@
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "semaphore.h"
+
+union semun {
+    int val;
+    struct semid_ds* buf;
+    unsigned short* array;
+};
+
+int create_semaphore(key_t key) {
+    int id = semget(key, 1, IPC_CREAT | 0666);
+    if (id == -1) {
+        perror("semget failed");
+        exit(1);
+    }
+    return id;
+}
+
+void init_semaphore(int semid, int value) {
+    union semun arg;
+    arg.val = value;
+    if (semctl(semid, 0, SETVAL, arg) == -1) {
+        perror("semctl init failed");
+        exit(1);
+    }
+}
+
+void sem_wait(int semid) {
+    struct sembuf op = {0, -1, 0};
+    if (semop(semid, &op, 1) == -1) {
+        perror("sem_wait failed");
+        exit(1);
+    }
+}
+
+void sem_signal(int semid) {
+    struct sembuf op = {0, 1, 0};
+    if (semop(semid, &op, 1) == -1) {
+        perror("sem_signal failed");
+        exit(1);
+    }
+}
